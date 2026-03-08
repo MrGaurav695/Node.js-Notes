42b00c529444308237140c6c260bf6437fa2975b
GAURAV RAJESH SHIRSATH <gauravshirsath911@gmail.com>
2026-03-08 15:01:19 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,86 @@
+//!                                         Introduction
+// javaScripts -> Browser
+// Node.js -> server
+
+// Node.js -> javascript runtime environment
+// Node.js is js runtime environment that allows js to run outside the browser on the server.
+
+// Node.js allow karta hai -> server banana, api banana, real-time apps, backend banana.
+
+// creator -> Ryan dahl
+// year -> 2009
+// engine -> google v8 engine
+
+// node js ne ky ky use krte hai
+// single thread
+// event driven atrchitecture
+// non-blocking
+
+// userRequest -> eventloop -> process requests
+// node js ek hi threas me multiple request handle karta
+
+// where node js is used
+// real time application 
+// rest apis -> get post put delete
+// stremin application -> video streaming
+// backend development
+
+// advantages of nodejs
+// fast executions -> google v8 engine
+// js -> machine code 
+
+// non-blocking I/O
+// 3 user request
+// 1->
+// 2-> w8
+// 3-> w8
+
+// single programming language
+// frontend -> js react angular
+// backend -> java/php/python
+
+// nodejs ->
+// frontend -> javascripts
+// backend -> javascripts
+// developer ko 1 language seekhni padti hai
+
+// large Ecosystem
+// npm (node package manager)
+// express, axios, lodash
+// npm install express
+
+// features             nodejs              vs              javascripts
+// environment          server                              Browser
+// file system          yes                                 no
+// operatingsystem      yes                                 no
+// backend dev          yes                                 no
+
+// readfile()
+// createserver()
+
+// console.log("Hello Sir");
+
+// node global objects
+// node.js me kuchh built-in variable hote hai
+
+// console.log(__dirname);
+// console.log("global objects: ");
+// console.log(__filename);
+// console.log(process.platform);
+
+// node js Architecure
+// based on -> event driven, non blocking, single thread
+// nodejs multiple request simultaneosly handel karta hai
+// without creating multiple thread
+
+// clints -> request queue -> eventloop -> worker thread -> response
+
+// user -> login
+// event loop
+// eventloop contineosly check karta hai request
+// agaer koi request aai hai to start karta hai
+
+// event loop is mechanism that contineously checks the request queues
+// and process events
+
+// event loop ek infinite loop hai jo check karta hai
