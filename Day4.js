7ea06e18341333fa191a898eb57e28628f15babd
GAURAV RAJESH SHIRSATH <gauravshirsath911@gmail.com>
2026-03-15 16:34:56 +0530

    Add files via upload

---
+++ Day4.js
@@ -0,0 +1,76 @@
+// --------------------------------------------------
+// 1. OS Module
+// --------------------------------------------------
+
+// os module system information deta hai
+
+console.log("os module:")
+
+const os = require("os");
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.cpus());
+
+console.log(os.totalmem());
+
+
+
+// --------------------------------------------------
+// 2. NPM (Node Package Manager)
+// --------------------------------------------------
+
+// npm stands for Node Package Manager
+
+// default package manager for Node.js
+
+// npm kya karta hai:
+
+// package install karta hai
+// package update karta hai
+// package remove karta hai
+// project dependencies manage karta hai
+
+
+
+// --------------------------------------------------
+// 3. Package
+// --------------------------------------------------
+
+// package kya hota hai
+
+// a package is reusable piece of code that developers
+// can install and use in their applications
+
+// examples
+
+// express
+// axios
+// lodash
+// mongoose
+// react
+
+
+
+// --------------------------------------------------
+// 4. NPM Registry
+// --------------------------------------------------
+
+// npm registry ek online database hai
+// jaha sab npm packages store hote hain
+
+
+
+// --------------------------------------------------
+// 5. package.json
+// --------------------------------------------------
+
+// package.json ek configuration file hai
+// jo project ki information aur dependencies store karti hai
+
+// stored information
+
+// project name
+// project version
+// dependencies
+// scripts
